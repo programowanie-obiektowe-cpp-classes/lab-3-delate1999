7dc0cbd5b0af6bb1c8c647d9c3388279e46817b4
MK <whatever@nevermind.com>
2022-11-17 20:31:48 +0100

    V1.0

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -5,5 +5,16 @@
 unsigned int transportujFlota(unsigned int towar)
 {
     // Twoja implementacja tutaj
-    return 0;
+    Stocznia stocznia{};
+    unsigned int przetransportowane = 0;
+    unsigned int liczba_zaglowcow = 0;
+    while(przetransportowane < towar){
+        Statek* s1 = stocznia();
+        przetransportowane += s1->transportuj();
+        if(dynamic_cast<Zaglowiec*>(s1) != nullptr){
+             liczba_zaglowcow++;
+        }
+        delete s1;
+    }
+    return liczba_zaglowcow;
 }
